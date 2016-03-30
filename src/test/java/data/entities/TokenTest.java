9633fe87d1c7d3cee8bfe5a3195a04ec6e74ed64
AdrianSantosSDG <adrian.santos@sdggroup.com>
2016-03-31 00:43:12 +0200

    TokenTest

--- src/test/java/data/entities/TokenTest.java
+++ src/test/java/data/entities/TokenTest.java
@@ -1,8 +1,10 @@
 package data.entities;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import org.junit.Test;
 
@@ -16,6 +18,17 @@ public class TokenTest {
         User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
         Token token = new Token(user);
         assertTrue(token.getValue().length() > 20);
+    }
+    
+    @Test
+    public void testTokenExpired() {
+        User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
+        Token token = new Token(user);
+        assertEquals(token.hasExpired(), false);
+        Calendar later = new GregorianCalendar();
+        later.add(Calendar.HOUR_OF_DAY, -2);
+        token.setCreationTime(later);
+        assertEquals(token.hasExpired(), true);
     }
 
 }
