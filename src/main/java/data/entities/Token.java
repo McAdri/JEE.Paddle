19f0f77971f42210d1667f47fbfcc0f68a31990c
AdrianSantosSDG <adrian.santos@sdggroup.com>
2016-03-31 00:05:14 +0200

    Expiracion Token

--- src/main/java/data/entities/Token.java
+++ src/main/java/data/entities/Token.java
@@ -1,5 +1,6 @@
 package data.entities;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -8,16 +9,27 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.hql.internal.ast.tree.BooleanLiteralNode;
 
 @Entity
 public class Token {
 
+	private static final int HOURS_EXPIRED = 1;
+	
     @Id
     @GeneratedValue
     private int id;
 
     @Column(unique = true, nullable = false)
     private String value;
+    
+    
+    @Column(nullable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar creationTime;
 
     @ManyToOne
     @JoinColumn
@@ -31,6 +43,8 @@ public class Token {
         this.user = user;
         this.value = new Encrypt().encryptInBase64UrlSafe("" + user.getId() + user.getUsername() + Long.toString(new Date().getTime())
                 + user.getPassword());
+        
+        this.creationTime = Calendar.getInstance();
     }
 
     public int getId() {
@@ -43,6 +57,21 @@ public class Token {
 
     public User getUser() {
         return user;
+    }
+    
+    public Calendar getCreationTime(){
+    	return creationTime;
+    }
+    
+    public void setCreationTime(Calendar creationTime){
+    	this.creationTime = creationTime;
+    }
+    
+    public boolean hasExpired(){
+    	Calendar nowTime = Calendar.getInstance();
+    	Calendar expirationTime = this.creationTime;
+    	expirationTime.add(Calendar.HOUR_OF_DAY, HOURS_EXPIRED);
+    	return (expirationTime.compareTo(nowTime) <= 0 );
     }
 
     @Override
